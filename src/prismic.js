db23dd0996f992d2511fdd06f1a6c7a5a95096bf
liam <lamtruong@panicbear.consulting>
2024-02-14 19:08:09 +0700

    update

---
+++ src/prismic.js
@@ -0,0 +1,15 @@
+import * as prismic from '@prismicio/client'
+
+const repositoryName = 'kanak-sample'
+const accessToken = 'MC5aY25QeVJBQUFDSUF2a3Ez.77-9OGBebe-_ve-_vVp277-9Ze-_ve-_vSfvv73vv711eEAB77-9eQpq77-977-9Vu-_vQDvv71s77-9'
+const routes = [
+    // Update to match your website's URL structure
+    // { type: 'page', path: '/:uid' },
+    { type: 'home', path: '/' },
+  ]
+const client = prismic.createClient(repositoryName)
+
+export function getPage(slug) {
+    return client.getSingle(slug)
+}
+
