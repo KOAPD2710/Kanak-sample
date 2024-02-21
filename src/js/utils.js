94686b11efd07af537297a89974f249051841e34
liam <lamtruong@panicbear.consulting>
2024-02-21 12:19:15 +0700

    update

--- src/js/utils.js
+++ src/js/utils.js
@@ -22,4 +22,12 @@ const getIndex = (el) => {
         return [...dom(el).parentNode.children].indexOf(dom(el))
     }
 }
-export { dom, addEvent, addEventAll, getIndex }
+const offset = (el) => {
+    let box = el.getBoundingClientRect();
+    let docElem = document.documentElement;
+    return {
+      top: box.top + window.scrollY - docElem.clientTop,
+      left: box.left + window.scrollX - docElem.clientLeft
+    };
+  }
+export { dom, addEvent, addEventAll, getIndex, offset }
