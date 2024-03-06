8ba0caaccca289a8da086b0cb672aeb149bdb2f8
KOAPD2710 <khoaphan27102000@gmail.com>
2024-03-06 18:36:46 +0700

    Update

---
+++ src/components/pages/Policy/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import './Hero.scss'
+
+function PolicyHero() {
+    return (
+        <>
+            <section className="policy-hero">
+                <div className="container grid">
+                    <h1 className='heading h0 txt-black txt-up policy-hero-title'>PRIVACY POLICY</h1>
+                    </div>
+            </section>
+        </>
+    )
+}
+
+export default PolicyHero
