e6d2258027725628f181afc48a64477ed7570755
liam <lamtruong@panicbear.consulting>
2024-03-12 18:09:43 +0700

    update model

--- src/components/pages/Home/Industry/Industry.jsx
+++ src/components/pages/Home/Industry/Industry.jsx
@@ -2,22 +2,22 @@ import './Industry.scss'
 
 const HomeIndustry = (props) => {
     return (
-        <section class="home-indus bg-dark">
-            <div class="container grid">
-                <div class="home-indus-title-wrap">
-                    <div class="home-indus-title-stick">
-                        <p class="heading h4 txt-up txt-black home-indus-label">{props.label}</p>
-                        <h2 class="heading h0 txt-up txt-black home-indus-title">{props.title}</h2>
-                        <div class="home-indus-cta-wrap">
-                            <a href="#" class="home-indus-cta">
-                                <div class="home-indus-cta-inner">{props.arrIcon}</div>
-                                <div class="home-indus-cta-outer">{props.ctaTxt}</div>
+        <section className="home-indus bg-dark">
+            <div className="container grid">
+                <div className="home-indus-title-wrap">
+                    <div className="home-indus-title-stick">
+                        <p className="heading h4 txt-up txt-black home-indus-label">{props.label}</p>
+                        <h2 className="heading h0 txt-up txt-black home-indus-title">{props.title}</h2>
+                        <div className="home-indus-cta-wrap">
+                            <a href="#" className="home-indus-cta">
+                                <div className="home-indus-cta-inner">{props.arrIcon}</div>
+                                <div className="home-indus-cta-outer">{props.ctaTxt}</div>
                             </a>
                         </div>
                     </div>
                 </div>
-                <div class="home-indus-main">
-                    <div class="home-indus-main-list">
+                <div className="home-indus-main">
+                    <div className="home-indus-main-list">
                         {props.listServices}
                     </div>
                 </div>
