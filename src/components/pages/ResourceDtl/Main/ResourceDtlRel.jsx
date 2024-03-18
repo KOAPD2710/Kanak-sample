bc91df3f8025bad3d13218e6cea81bffc34e89c3
Phan Đăng Khoa <89030592+KOAPD2710@users.noreply.github.com>
2024-03-18 16:06:20 +0700

    easad

--- src/components/pages/ResourceDtl/Main/ResourceDtlRel.jsx
+++ src/components/pages/ResourceDtl/Main/ResourceDtlRel.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useKeenSlider } from 'keen-slider/react'
 import "keen-slider/keen-slider.min.css"
 import { convertDate } from "@utils/text.js"
@@ -7,6 +7,7 @@ import { convertDate } from "@utils/text.js"
 function ResDtlRel(props) {
     const [loaded, setLoaded] = useState(false);
     const [currentSlide, setCurrentSlide] = useState(0);
+    const [limit, setLimit] = useState(9);
     let perView = 2;
     let newList = props.list.reduce((accumulator, currentValue, currentIndex, array) => {
         if (currentIndex % perView === 0) {
@@ -16,7 +17,7 @@ function ResDtlRel(props) {
     }, [])
     const [sliderRef, instanceRef] = useKeenSlider({
         initial: 0,
-        disabled: flase,
+        disabled: false,
         slides: {
             spacing: 36,
         },
@@ -24,7 +25,7 @@ function ResDtlRel(props) {
             duration: 800
         },
         breakpoints: {
-            '(max-width: 767px)': {
+            '(max-width: 991px)': {
                 disabled: true
             },
         },
@@ -35,6 +36,10 @@ function ResDtlRel(props) {
             setLoaded(true)
         },
     })
+    useEffect(() => {
+
+        setLimit(1)
+    }, [])
     return (
         <div className="resource-dtl-rel">
             <div className="resource-dtl-rel-head">
@@ -61,38 +66,52 @@ function ResDtlRel(props) {
             <div className="resource-dtl-rel-main">
                 <div className="line"></div>
                 <div className="keen-slider  resource-dtl-rel-main-inner" ref={sliderRef} style={{ '--perView': perView }}>
-                    {newList.map((chunk, idx) => (
-                        <div className="keen-slider__slide resource-dtl-rel-main-inner-group" key={idx}>
-                            {chunk.map((item, itemIdx) => (
-                                <a href={`/resources/${item.uid}`} className="resource-dtl-rel-main-inner-group-item" key={itemIdx} >
-                                    <div className="resource-dtl-rel-main-inner-group-item-img">
-                                        <img src={item.data.feature_image.url} alt={item.data.feature_image.alt} width={item.data.feature_image.dimensions.width} className='img img-fill' />
-                                    </div>
-                                    <div className="resource-dtl-rel-main-inner-group-item-content">
-                                        <div className="txt txt-20 txt-bold resource-dtl-rel-main-inner-group-item-content-cate">
-                                            {item.data.category}
+                    {newList.map((chunk, idx) =>
+                        idx < limit && (
+                            <div className="keen-slider__slide resource-dtl-rel-main-inner-group" key={idx}>
+                                {chunk.map((item, itemIdx) => (
+                                    <a href={`/resources/${item.uid}`} className="resource-dtl-rel-main-inner-group-item" key={itemIdx} >
+                                        <div className="resource-dtl-rel-main-inner-group-item-img">
+                                            <img src={item.data.feature_image.url} alt={item.data.feature_image.alt} width={item.data.feature_image.dimensions.width} className='img img-fill' />
                                         </div>
-                                        <h3 className='heading h4 txt-black txt-up resource-dtl-rel-main-inner-group-item-content-title'>
-                                            {item.data.title}
-                                        </h3>
-                                        <p className='txt txt-18 txt-med resource-dtl-rel-main-inner-group-item-content-des'>
-                                            {item.data.sapo}
-                                        </p>
-                                        <span className='txt txt-18 txt-med resource-dtl-rel-main-inner-group-item-content-date'>
-                                            {convertDate(item.last_publication_date)}
-                                        </span>
-                                        {itemIdx % 2 !== 0 && (
-                                            <>
-                                                <div className="line line-ver"></div>
-                                                <div className="line"></div>
-                                            </>
-                                        )}
-                                    </div>
-                                </a>
-                            ))}
-                        </div>
-                    ))}
+                                        <div className="resource-dtl-rel-main-inner-group-item-content">
+                                            <div className="txt txt-20 txt-bold resource-dtl-rel-main-inner-group-item-content-cate">
+                                                {item.data.category}
+                                            </div>
+                                            <h3 className='heading h4 txt-black txt-up resource-dtl-rel-main-inner-group-item-content-title'>
+                                                {item.data.title}
+                                            </h3>
+                                            <p className='txt txt-18 txt-med resource-dtl-rel-main-inner-group-item-content-des'>
+                                                {item.data.sapo}
+                                            </p>
+                                            <span className='txt txt-18 txt-med resource-dtl-rel-main-inner-group-item-content-date'>
+                                                {convertDate(item.last_publication_date)}
+                                            </span>
+                                            {itemIdx % 2 !== 0 && (
+                                                <>
+                                                    <div className="line line-ver"></div>
+                                                    <div className="line"></div>
+                                                </>
+                                            )}
+                                        </div>
+                                    </a>
+                                ))}
+                            </div>
+                        )
+                    )}
                 </div>
+            </div>
+            <div className={`resource-dtl-rel-load ${limit >= newList.length ? 'hidden' : ''}`}>
+                <button className="resource-dtl-rel-load-btn" onClick={() => setLimit(limit + 4)}>
+                    <div className="resource-dtl-rel-load-btn-ic">
+                        <div className="ic ic-24">
+                            {props.icArrowDown}
+                        </div>
+                    </div>
+                    <div className="txt txt-20 txt-med resource-dtl-rel-load-btn-txt">
+                        Load more
+                    </div>
+                </button>
             </div>
         </div >
     )
