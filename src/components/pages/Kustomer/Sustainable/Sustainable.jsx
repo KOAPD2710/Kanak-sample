7ec2f7d4fa9cb220a99a0d1ca4bcb8db66a7c4ad
Phan Đăng Khoa <89030592+KOAPD2710@users.noreply.github.com>
2024-03-28 10:22:59 +0700

    Update Sustan

--- src/components/pages/Kustomer/Sustainable/Sustainable.jsx
+++ src/components/pages/Kustomer/Sustainable/Sustainable.jsx
@@ -15,6 +15,43 @@ function KustomerSustain({ ...props }) {
                         our premier line of dinnerware crafted from premium sugarcane bagasse.
                     </span>
                 </div>
+                <div className="kustomer-sus-main">
+                    <div className="line kustomer-sus-main-line-top"></div>
+                    <div className="line line-ver kustomer-sus-main-line-right"></div>
+                    <div className="kustomer-sus-main-cate">
+                        <ul className="kustomer-sus-main-cate-list">
+                            <li className="kustomer-sus-main-cate-list-item">
+                                <a href="" className="kustomer-sus-main-cate-list-item-inner">
+                                    <div className="dot"></div>
+                                    <span className="heading h6 txt-black txt-up kustomer-sus-main-cate-item-txt">Bowl</span>
+                                </a>
+                            </li>
+                            <li className="kustomer-sus-main-cate-list-item">
+                                <a href="" className="kustomer-sus-main-cate-list-item-inner">
+                                    <div className="dot"></div>
+                                    <span className="heading h6 txt-black txt-up kustomer-sus-main-cate-item-txt">Plates & Platters</span>
+                                </a>
+                            </li>
+                            <li className="kustomer-sus-main-cate-list-item">
+                                <a href="" className="kustomer-sus-main-cate-list-item-inner">
+                                    <div className="dot"></div>
+                                    <span className="heading h6 txt-black txt-up kustomer-sus-main-cate-item-txt">Cups</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="kustomer-sus-main-table">
+                        <a href="#" className="kustomer-sus-main-table-item">
+                            <div className="kustomer-sus-main-table-item-img">
+                                {props.img}
+                            </div>
+                            <div className="kustomer-sus-main-table-item-info">
+                                <h4 className="heading h5 kustomer-sus-main-table-item-info-name"></h4>
+                            </div>
+                        </a>
+
+                    </div>
+                </div>
             </div>
         </section>
     )
