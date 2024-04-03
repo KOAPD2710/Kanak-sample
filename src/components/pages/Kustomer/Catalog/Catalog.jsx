ac72dde11a7845b8f6624645ee61508efca697f3
Phan Đăng Khoa <89030592+KOAPD2710@users.noreply.github.com>
2024-04-03 19:20:41 +0700

    alabatrap

--- src/components/pages/Kustomer/Catalog/Catalog.jsx
+++ src/components/pages/Kustomer/Catalog/Catalog.jsx
@@ -2,16 +2,16 @@ import "./Catalog.scss"
 
 import CatalogMain from "./CatalogMain"
 
-function KustomerCatalog({ label, title, describe, ...props }) {
+function KustomerCatalog({ ...props }) {
     return (
         <section className="kustomer-cata">
             <div className="container grid">
                 <div className="kustomer-cata-title-wrap">
-                    <div className="heading h3 txt-black txt-up kustomer-cata-label">{label}</div>
-                    <h1 className="heading h0 txt-black txt-up kustomer-cata-title">{title}</h1>
+                    <div className="heading h3 txt-black txt-up kustomer-cata-label">{props.label}</div>
+                    <h1 className="heading h0 txt-black txt-up kustomer-cata-title">{props.title}</h1>
                 </div>
                 <div className="kustomer-cata-des">
-                    <p className="txt txt-18 txt-med">{describe}</p>
+                    <p className="txt txt-18 txt-med">{props.describe}</p>
                 </div>
                 <CatalogMain arrIcon={props.arrIcon} qr={props.qr} img={props.image} list={props.data} {...props} />
             </div>
